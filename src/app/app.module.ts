12d8c99d0df7670cc7489a79f03c4fa460b451f8
mstoews <mstoews@hotmail.com>
2022-07-07 15:01:17 +0900

    Update initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ClipboardComponent } from './clipboard/clipboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscribeComponent,
+    LandingPageComponent,
+    ClipboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clipboard/clipboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clipboard',
+  templateUrl: './clipboard.component.html',
+})
+export class ClipboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
